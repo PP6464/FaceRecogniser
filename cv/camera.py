8cc50f5b36169b8640593592ccea683e6a0c28fa
PP6464 <panthhp@outlook.com>
2024-09-17 15:33:20 +0100

    see and analyse a face

---
+++ cv/camera.py
@@ -0,0 +1,53 @@
+import cv2
+
+from cv.face_processor import process_face
+
+# Detect faces using a pre-trained face feature recognising classifier
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+cap = cv2.VideoCapture(0)
+
+# Exit if you can't open camera
+if not cap.isOpened():
+    print("Can't open camera")
+    exit()
+
+
+# Continuously load frames from the camera
+while True:
+    ret, frame = cap.read()
+
+    # Break out if you can't load frames
+    if not ret:
+        print("Failed to grab frame")
+        break
+
+    # Convert to grayscale for better Haar Cascade detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for x, y, w, h in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)  # Draw a blue rect (it is in BGR) of thickness 2
+        if frame_count == 0:
+            prediction = process_face(frame, (x, y, w, h))
+            cv2.putText(
+                frame,
+                "Me" if prediction == 0 else "Not me",
+                (x, y-10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.9,
+                (255, 255, 255),
+                2,
+            )
+
+    cv2.imshow("Camera", frame)
+
+    # Exit the loop if the 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Step 3: Release the camera and close any open windows
+cap.release()
+cv2.destroyAllWindows()
+
---
+++ cv/face_processor.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+from keras.src.saving import load_model
+
+
+def process_face(image, face_coords) -> int:
+    x, y, w, h = face_coords
+    face = image[y:y+h, x:x+w]
+
+    face_resized = cv2.resize(face, (224, 224))
+
+    face_array = np.array(face_resized)
+
+    model = load_model("../nn/model/model.keras")
+    raw_prediction = model.predict(face_array[None, :, :])
+
+    P_me = raw_prediction[0][0]  # Probability it is me
+    P_not_me = raw_prediction[0][1]  # Probability it is not me
+
+    if P_not_me >= P_me:
+        return 1  # Same as the label for 'not me'
+    else:
+        return 0  # Same as the label for 'me'
