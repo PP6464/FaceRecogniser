2b797572847e6c4bea9df9d7ea11f5baaf91abde
PP6464 <panthhp@outlook.com>
2024-09-17 10:41:48 +0100

    test using the model

---
+++ nn/model_user.py
@@ -0,0 +1,9 @@
+# This is just to test loading the model and making a prediction with it
+from PIL import Image
+from keras.src.saving.saving_lib import load_model
+import numpy as np
+
+model = load_model("model/model.keras")
+test_image = Image.open("data/me/me20.png").convert('RGB').resize((224, 224), Image.Resampling.LANCZOS)
+test_image = np.array(test_image)
+print(model.predict(test_image[None, :, :]))
