7b43bb7911f68ed47d0d634bd84cf3e69694e54a
PP6464 <panthhp@outlook.com>
2024-10-17 19:05:28 +0100

    sort all data imgs by likeness to me

--- nn/model_user.py
+++ nn/model_user.py
@@ -4,6 +4,19 @@ from keras.src.saving.saving_lib import load_model
 import numpy as np
 
 model = load_model("model/model.keras")
-test_image = Image.open("data/me/me55.png").convert('L').resize((512, 512), Image.Resampling.LANCZOS)
-test_image = np.array(test_image)
-print(model.predict(test_image[None, :, :]))
+
+test_images = []
+for i in range(60):
+    img_me = Image.open(f"data/me/me{i+1}.png").convert('L').resize((512, 512), Image.Resampling.LANCZOS)
+    img_not_me = Image.open(f"data/notme/notme{i+1}.png").convert('L').resize((512, 512), Image.Resampling.LANCZOS)
+
+    test_images.extend([np.array(img_me), np.array(img_not_me)])
+
+
+for index, img in enumerate(test_images):
+    test_images[index] = (img, model.predict(img[None, :, :])[0][0], index)
+
+
+test_images.sort(key=lambda x: x[1])
+
+print(test_images[-1][2] + 1)  # The image that looks the most like me
